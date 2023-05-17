f0eb7694ed4576441c6e9c407cb7fd47140353c2
Julian <julianser@hotmail.com.ar>
2023-05-17 10:39:53 -0300

    agregue el maquetado de app

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Container } from 'react-bootstrap'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
@@ -5,7 +6,14 @@ function App() {
 
   return (
     <>
-      <h1>Titulo de prueba</h1>
+    <Container className='my-5 mainPage'>
+      <h1 className='display-4 text-center text-light'>Lista de Tareas</h1>
+      <hr className='text-light'/>
+      {/* aqui agrego el formulario*/}
+    </Container>
+    <footer className='bg-dark text-light py-4 text-center'>
+      <p>&copy; Todos los derechos reservados</p>
+    </footer>
     </>
   )
 }
